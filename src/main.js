f2f822e5d9df9cf6a1544ec68701c2dd10d47bd3
coolb <jingshuai7687@gmail.com>
2023-12-15 18:25:02 +0800

    add intp

--- src/main.js
+++ src/main.js
@@ -6,6 +6,6 @@ import {createPinia} from 'pinia'
 import i18n from './lang/index'
 const store = createPinia()
 if(!navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)){
-    window.location.href= 'http://www.qdtz888.com/'; 
+    window.location.href= 'https://intp.qdtz888.com/'; 
 }
 createApp(App).use(store).use(i18n).use(router).mount('#app')
